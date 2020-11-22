91603384f9e6c9dcc5c1216282b6a01feff2bc4c
Ivan Ilchenko <differentstuff777@gmail.com>
2020-11-22 20:47:23 +0300

    Some progress on fattree cont-net

--- fattree-cont.py
+++ fattree-cont.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 
-from mininet.net import Mininet
-from mininet.topo import Topo
+#  from mininet.net import Mininet
+from mininet.net import Containernet
+from mininet.node import Controller
+#  from mininet.topo import Topo
 from mininet.link import TCLink
 from mininet.cli import CLI
 import logging
@@ -10,28 +12,30 @@ import os
 logging.basicConfig(filename='./fattree.log', level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-class FatTree( Topo ):
+#  class FatTree( Topo ):
+class FatTree:
 
     CoreSwitchList = []
     AggSwitchList = []
     EdgeSwitchList = []
     HostList = []
 
-    def __init__( self, k):
+    def __init__( self, k, net):
         " Create Fat Tree topo."
-        self.pod = k
-        self.iCoreLayerSwitch = (k/2)**2
-        self.iAggLayerSwitch = k*k/2
-        self.iEdgeLayerSwitch = k*k/2
-        self.density = k/2
+        self.pod = int(k)
+        self.iCoreLayerSwitch = int((k/2)**2)
+        self.iAggLayerSwitch = int(k*k/2)
+        self.iEdgeLayerSwitch = int(k*k/2)
+        self.density = int(k/2)
         self.iHost = self.iEdgeLayerSwitch * self.density
 
         self.bw_c2a = 0.2
         self.bw_a2e = 0.1
         self.bw_h2a = 0.05
+        self.net = net
 
         # Init Topo
-        Topo.__init__(self)
+        #  Topo.__init__(self)
 
         self.createTopo()
         logger.debug("Finished topology creation!")
@@ -55,11 +59,11 @@ class FatTree( Topo ):
     """
 
     def _addSwitch(self, number, level, switch_list):
-        for x in xrange(1, number+1):
+        for x in range(1, number+1):
             PREFIX = str(level) + "00"
             if x >= int(10):
                 PREFIX = str(level) + "0"
-            switch_list.append(self.addSwitch('s' + PREFIX + str(x)))
+            switch_list.append(self.net.addSwitch('s' + PREFIX + str(x)))
 
     def createCoreLayerSwitch(self, NUMBER):
         logger.debug("Create Core Layer")
@@ -75,23 +79,25 @@ class FatTree( Topo ):
 
     def createHost(self, NUMBER):
         logger.debug("Create Host")
-        for x in xrange(1, NUMBER+1):
+        for x in range(1, NUMBER+1):
             PREFIX = "h00"
             if x >= int(10):
                 PREFIX = "h0"
             elif x >= int(100):
                 PREFIX = "h"
-            self.HostList.append(self.addHost(PREFIX + str(x)))
+            #  self.HostList.append(self.addHost(PREFIX + str(x)))
+            image="spagnuolocarmine/docker-mpi"
+            self.HostList.append(self.net.addDocker(PREFIX + str(x), dimage=image, volumes=["data:/data"]))
 
     """
     Add Link
     """
     def createLink(self, bw_c2a=0.2, bw_a2e=0.1, bw_h2a=0.5):
         logger.debug("Add link Core to Agg.")
-        end = self.pod/2
-        for x in xrange(0, self.iAggLayerSwitch, end):
-            for i in xrange(0, end):
-                for j in xrange(0, end):
+        end = int(self.pod/2)
+        for x in range(0, self.iAggLayerSwitch, end):
+            for i in range(0, end):
+                for j in range(0, end):
                     core_ind = i * end + j
                     agg_ind = x + i
                     linkopts = dict(bw=bw_c2a)
@@ -100,35 +106,69 @@ class FatTree( Topo ):
                         bw_damaged = bw_c2a #TODO: it should be some other value
                         linkopts = dict(bw=bw_damaged)
 
-                    self.addLink(
+                    #  self.net.addLink(
+                        #  self.CoreSwitchList[core_ind],
+                        #  self.AggSwitchList[agg_ind],
+                        #  **linkopts)
+                    self.net.addLink(
                         self.CoreSwitchList[core_ind],
-                        self.AggSwitchList[agg_ind],
-                        **linkopts)
+                        self.AggSwitchList[agg_ind])
 
         logger.debug("Add link Agg to Edge.")
-        for x in xrange(0, self.iAggLayerSwitch, end):
-            for i in xrange(0, end):
-                for j in xrange(0, end):
+        for x in range(0, self.iAggLayerSwitch, end):
+            for i in range(0, end):
+                for j in range(0, end):
                     linkopts = dict(bw=bw_a2e)
-                    self.addLink(
-                        self.AggSwitchList[x+i], self.EdgeSwitchList[x+j],
-                        **linkopts)
+                    #  self.net.addLink(
+                        #  self.AggSwitchList[x+i], self.EdgeSwitchList[x+j],
+                        #  **linkopts)
+                    self.net.addLink(
+                        self.AggSwitchList[x+i], self.EdgeSwitchList[x+j])
 
         logger.debug("Add link Edge to Host.")
-        for x in xrange(0, self.iEdgeLayerSwitch):
-            for i in xrange(0, self.density):
+        for x in range(0, self.iEdgeLayerSwitch):
+            for i in range(0, self.density):
                 linkopts = dict(bw=bw_h2a)
-                self.addLink(
+                #  self.net.addLink(
+                    #  self.EdgeSwitchList[x],
+                    #  self.HostList[self.density * x + i],
+                    #  **linkopts)
+                self.net.addLink(
                     self.EdgeSwitchList[x],
-                    self.HostList[self.density * x + i],
-                    **linkopts)
+                    self.HostList[self.density * x + i])
+
+        self.net.ping([self.HostList[0], self.HostList[1]])
+
+def dump_etc_hosts(net):
+    f = open(os.getenv('VOLUME') + '/etc_hosts', "a")
+    for d in net.hosts:
+        f.write(d.IP() + ' ' + d.name + '\n')
+    f.close()
+
+def dump_mpi_hosts_file(net):
+    f = open(os.getenv('VOLUME') + '/mpi_hosts_file', "a")
+    for d in net.hosts:
+        f.write(d.name + '\n')
+    f.close()
+
+def run_set_ssh(net):
+    for d in net.hosts:
+        d.cmd('/data/set_ssh.sh start')
 
 def main():
     k = 4
-    topo = FatTree(k)
+    #  topo = FatTree(k)
     #  net = Mininet(topo, )
-    net = Mininet(topo, link=TCLink)
+    #  net = Mininet(topo, link=TCLink)
+    net = Containernet(controller=Controller)
+    net.addController('c0')
+    topo = FatTree(k, net)
     net.start()
+
+    dump_etc_hosts(net)
+    dump_mpi_hosts_file(net)
+    run_set_ssh(net)
+
     CLI(net)
     net.stop()
 
