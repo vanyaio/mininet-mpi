1bf070c3202dfc9bc11ba8356c7f2afebc7fdf89
Dmitry <dima123-99@mail.ru>
2020-12-03 12:24:58 +0300

    Added CI to run the simulation. First version.

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+repository_dir=$PWD
+
+echo -e "\e[33mInstall containernet\e[0m"
+cd ..
+git clone https://github.com/containernet/containernet.git
+
+echo -e "\e[33mMove repository\e[0m"
+cp -r $repository_dir ./containernet/mininet-mpi
+
+echo -e "\e[33mInstall pox\e[0m"
+cd containernet
+git clone https://github.com/noxrepo/pox
+cd pox
+git checkout remotes/origin/dart
+cd ..
+
+echo -e "\e[33mReturn to repository\e[0m"
+cd mininet-mpi
